e187831755efcfad26dab51de2b65c96e4f389e0
timosha <tinaatimkovaa@gmail.com>
2020-03-13 01:41:34 -0400

    Fixed spelling mistake on main page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const Home = props => {
   return (
     <Fragment>
-      <h1>{'Can\'t figure out how to keep track your monthly spendings?'}</h1>
+      <h1>{'Can\'t figure out how to keep track of your monthly spendings?'}</h1>
       <div className='home-page-img'>
         <img src={ require('./Home_page.png') } />
       </div>
