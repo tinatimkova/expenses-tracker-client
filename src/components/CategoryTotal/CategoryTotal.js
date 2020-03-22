2c5e45a5ff12efeddd7f6bf4b33137bc2e1aad54
timosha <tinaatimkovaa@gmail.com>
2020-03-21 23:42:41 -0400

    Applied Number method for category total

--- src/components/CategoryTotal/CategoryTotal.js
+++ src/components/CategoryTotal/CategoryTotal.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CategoryTotal = props => {
   let total = 0
   props.category.map(function (transaction) {
-    total = total + parseFloat(transaction.amount)
+    total = total + Number(transaction.amount)
   })
 
   return (
